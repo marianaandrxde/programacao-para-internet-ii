9c07217d3fdf9ac0570b49975cef0fdb434a040d
Mariana <mariana.olivandrade@gmail.com>
2024-10-19 19:01:25 -0300

    feat: add montadora crud

--- patrocars-web-tradicional/main.py
+++ patrocars-web-tradicional/main.py
@@ -15,13 +15,14 @@ from view_models import InputMontadora
 app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
 
 templates = Jinja2Templates(directory='templates')
 
 # Montadora
 SQLModel.metadata.create_all(get_engine())
 
-montadoras = ['FIAT', 'Toyota', 'Nissan', 'Hyundai']
 montadoras: list[Montadora] = []
 
 repository = MontadoraRepository()
@@ -43,30 +44,38 @@ def montadora_form(request: Request):
   return templates.TemplateResponse(request, 'montadora_form.html')
 
 
-class MontadoraForm(BaseModel):
-  nome: str
+@app.get('/montadora_edit/{montadora_id}')
+async def montadora_form(request: Request, montadora_id: int):
+    montadora = repository.get_by_id(montadora_id)  # Obtém a montadora pelo ID
+    if not montadora:
+        return RedirectResponse('/montadoras_list', status_code=404)  # Ou outra lógica de erro
+    return templates.TemplateResponse("montadora_edit.html", {"request": request, "montadora": montadora})
+
+
+@app.get('/montadora_delete/{montadora_id}')
+async def montadora_form(request: Request, montadora_id: int):
+    montadora = repository.get_by_id(montadora_id)  # Obtém a montadora pelo ID
+    if not montadora:
+        return RedirectResponse('/montadoras_list', status_code=404)  # Ou outra lógica de erro
+    return templates.TemplateResponse("montadora_delete.html", {"request": request, "montadora": montadora})
 
 
 @app.post('/montadora_save')
 def montadora_save(request: Request, input: Annotated[InputMontadora, Form()]):
-  # Check --> pydantic model_dump
   montadora = Montadora(nome=input.nome, pais=input.pais, ano_fundacao=input.ano)
   repository.save(montadora)
   return RedirectResponse('/montadoras_list', status_code=303)
 
 
+@app.post('/montadora_edit/{montadora_id}')
+async def montadora_edit(montadora_id: int, request: Request, input: Annotated[InputMontadora, Form()]):
+    montadora = Montadora(nome=input.nome, pais=input.pais, ano_fundacao=input.ano)
+    repository.update(montadora_id, montadora)
+    return RedirectResponse('/montadoras_list', status_code=303)
 
 
-
-@app.get('/hello', response_class=HTMLResponse)
-def hello(request: Request):
-  return templates.TemplateResponse(
-    request, 'index.html', context={'name': 'João'}
-  )
-
-
-@app.post('/saudacao')
-def saudacao(request: Request, name: str = Form(...)):
-  return templates.TemplateResponse(
-    request, 'index.html', context={'name': name}
-  )
+@app.post('/montadora_delete/{montadora_id}')
+async def montadora_delete(montadora_id: int, request: Request, input: Annotated[InputMontadora, Form()]):
+    montadora = Montadora(nome=input.nome, pais=input.pais, ano_fundacao=input.ano)
+    repository.delete(montadora_id, montadora)  
+    return RedirectResponse('/montadoras_list', status_code=303)
--- patrocars-web-tradicional/persistence/montadora_repository.py
+++ patrocars-web-tradicional/persistence/montadora_repository.py
@@ -12,8 +12,39 @@ class MontadoraRepository():
     montadoras = self.session.exec(sttm).all()
     return montadoras
   
+  def get_by_id(self, montadora_id: int) -> Montadora:
+    return self.session.get(Montadora, montadora_id)
+
+  
   def save(self, montadora: Montadora):
     self.session.add(montadora)
     self.session.commit()
     self.session.refresh(montadora)
     return montadora
+  
+
+  def update(self, montadora_id: int, updated_montadora: Montadora):
+        # Buscando a montadora existente
+        existing_montadora = self.session.get(Montadora, montadora_id)
+        if existing_montadora:
+            # Atualizando os campos desejados
+            existing_montadora.nome = updated_montadora.nome
+            existing_montadora.pais = updated_montadora.pais
+            existing_montadora.ano_fundacao = updated_montadora.ano_fundacao
+            
+            # Comitando as alterações
+            self.session.commit()
+            self.session.refresh(existing_montadora)
+            return existing_montadora
+        else:
+            raise ValueError(f"Montadora com ID {montadora_id} não encontrada.")
+        
+  def delete(self, montadora_id: int, montadora: Montadora):
+        montadora = self.session.get(Montadora, montadora_id)
+        if not montadora:
+           raise ValueError(f"Montadora com ID {montadora_id} não encontrado.")
+        else:
+          self.session.delete(montadora)
+          self.session.commit()
+          return {"ok": True}
+
